5fd40267efffa73af584f3958663ba59683d1eed
Daniel Costas Rodríguez <daniel.costas.rodriguez@bootcamp-upgrade.com>
2024-09-26 21:01:09 +0200

    Add 001_AppJava2, réplica del 00_AppJava

---
+++ 001_AppJava2/app/src/main/java/com/example/a001_appjava2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a001_appjava2;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.boton1);
+
+        b.setOnClickListener(view -> {
+            Log.i("TAAAAAG", "HOLA POR CONSOLA");
+        });
+    }
+}
