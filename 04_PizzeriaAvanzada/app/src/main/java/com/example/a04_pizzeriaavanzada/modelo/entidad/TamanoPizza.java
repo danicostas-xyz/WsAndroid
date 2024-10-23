d9f8a7be93e69a337e015c57a36776e93a0fc56a
Dani Costas <daniel.costas.rodriguez@bootcamp-upgrade.com>
2024-10-23 20:53:49 +0200

    Add 04_PizzeriaAvanzada incomplete

---
+++ 04_PizzeriaAvanzada/app/src/main/java/com/example/a04_pizzeriaavanzada/modelo/entidad/TamanoPizza.java
@@ -0,0 +1,5 @@
+package com.example.a04_pizzeriaavanzada.modelo.entidad;
+
+public enum TamanoPizza {
+    GRANDE, MEDIANA, PEQUENA
+}
---
+++ 04_PizzeriaAvanzada/app/src/main/java/com/example/a04_pizzeriaavanzada/modelo/negocio/GestorUsuario.java
@@ -0,0 +1,8 @@
+package com.example.a04_pizzeriaavanzada.modelo.negocio;
+
+public class GestorUsuario {
+
+
+
+
+}
